72846bb8cd6adc45007f5c2d36ff7f353a687ef4
aritroCoder <ogcodinggod@gmail.com>
2023-12-11 11:54:29 +0530

    removed header network switcher
    Signed-off-by: aritroCoder <ogcodinggod@gmail.com>

--- packages/site/src/components/Header.tsx
+++ packages/site/src/components/Header.tsx
@@ -87,57 +87,7 @@ export const Header = ({
         <Title>SNAPTOS</Title>
       </LogoWrapper>
       <RightContainer>
-        {/* <div
-          style={{
-            font: 'Roboto',
-            fontSize: '1.6rem',
-            border: '1px solid #000',
-            
-            borderRadius: '10px',
-            width: '10rem',
-            height: '3.4rem',
-            textAlign: 'center',
-            lineHeight: '3rem',
-            display: 'inline-block',
-          }}
-        >
-          {selectedNetwork.toUpperCase()}{' '}
-          <ArrowDropDownCircleOutlinedIcon
-            style={{
-              fontSize: 15,
-              color: '#434343',
-              lineHeight: '3rem',          
-              display: 'inline-block'
-            }}
-            onClick={handleDropdownClick}
-          />
-        </div>
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleDropdownClose}
-        >
-          <MenuItem
-            onClick={() => handleNetworkSelect('mainnet')}
-            sx={{ fontSize: '15px', font: 'Roboto' }}
-          >
-            Mainnet
-          </MenuItem>
-          <MenuItem
-            onClick={() => handleNetworkSelect('testnet')}
-            sx={{ fontSize: '15px', font: 'Roboto' }}
-          >
-            Testnet
-          </MenuItem>
-          <MenuItem
-            onClick={() => handleNetworkSelect('devnet')}
-            sx={{ fontSize: '15px', font: 'Roboto' }}
-          >
-            Devnet
-          </MenuItem>
-        </Menu>{' '}
-        <div style={{ marginRight: '20px' }} /> */}
-        <Network/>
+        {/* <Network/> */}
         <Toggle
           onToggle={handleToggleClick}
           defaultChecked={getThemePreference()}
